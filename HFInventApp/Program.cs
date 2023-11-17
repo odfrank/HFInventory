8e2b90d27ed4923044c912715e444e96a06ca522
odfrank <oyedeledamilaref@yahoo.com>
2023-11-17 02:23:21 -0700

    Adds User Authorization for Login and Register UIs

--- HFInventApp/Program.cs
+++ HFInventApp/Program.cs
@@ -13,6 +13,14 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
 
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = $"/Identity/Account/Login";
+    options.LogoutPath = $"/Identity/Account/Logout";
+    options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+});
+
+
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<IEmailSender, EmailSender>();
 
