eb4727836dd95421d9b9fc102bf1fba92936ec04
odfrank <oyedeledamilaref@yahoo.com>
2023-11-17 02:21:26 -0700

    Adds Roles Data and EmailSender Class

--- HFInventApp/Program.cs
+++ HFInventApp/Program.cs
@@ -1,6 +1,8 @@
 using HFInventApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using HFInventApp.Utility;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,9 +11,10 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("HFConn")));
 
-builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<AppDbContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
 
 builder.Services.AddRazorPages();
+builder.Services.AddScoped<IEmailSender, EmailSender>();
 
 
 var app = builder.Build();
